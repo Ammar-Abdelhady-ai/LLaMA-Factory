3943efbaf8114cede0a16051ebcf1a97038b4ab3
Ammar-Abdelhady-ai <ammarabdelhady8@gmail.com>
2024-07-12 16:15:47 +0300

    add files

---
+++ src/webui.py
@@ -0,0 +1,34 @@
+# Copyright 2024 the LlamaFactory team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import socket
+
+from llamafactory.webui.interface import create_ui
+
+
+def main():
+    # Determine an available port dynamically
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost', 0))
+    _, port = sock.getsockname()
+    sock.close()
+
+    gradio_share = os.environ.get("GRADIO_SHARE", "0").lower() in ["true", "1"]
+    server_name = os.environ.get("GRADIO_SERVER_NAME", f"0.0.0.0:{port}")
+    create_ui().queue().launch(share=True, server_name=server_name, inbrowser=True)
+
+
+if __name__ == "__main__":
+    main()
